48f960b9197c2d0111102ef79092466e43931c13
Andrew Pochapsky <andrew.pochapsky@gmail.com>
2016-12-10 00:55:10 -0500

    basic movement, there is jump bug as well as minor visual bug with sprite sheet for player

---
+++ Assets/Scripts/DetectFloor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectFloor : MonoBehaviour {
+    public static bool canJump = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print("canJump=" + canJump);
+	}
+
+    bool OnTriggerEnter2D(Collider2D col)
+    {
+        print("tag is "+ col.tag);
+        
+        if (col.tag == "Floor")
+        {
+            canJump = true;
+        }
+        else
+        {
+            canJump = false;
+        }
+        return canJump;
+
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public int speed = 5;
+    public int jumpStrength=10;
+    private Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Move();
+        
+    }
+
+    void Move()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+
+        if (DetectFloor.canJump && Input.GetKeyDown(KeyCode.W))
+        {
+            rb.AddForce(Vector2.up * jumpStrength);
+            DetectFloor.canJump = false;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+        //if (Input.GetKey(KeyCode.S))
+        //{
+        //    transform.Translate(Vector2.down * speed * Time.deltaTime);
+        //}
+    }
+
+
+
+
+}
